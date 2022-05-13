4c76899480c575d9950762887ee903862de3f404
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2022-05-13 13:24:04 +0200

    Loading driver.

--- bpfcap_fpga/bpfcap_fpga.c
+++ bpfcap_fpga/bpfcap_fpga.c
@@ -10,7 +10,7 @@
 
 // TODO: need a define for the physical emory address we will have assigned
 //
-#define BPFCAP_FPGA_MEM_START   0x1000000
+#define BPFCAP_FPGA_MEM_START   0x1000000 // FIXME: add proper address?
 #define BPFCAP_FPGA_CTRL        0x00 /* Read / Write */
 #define BPFCAP_FPGA_PKT_BEGIN   0x04 /* Write */
 #define BPFCAP_FPGA_PKT_END     0x08 /* Write */
@@ -33,7 +33,7 @@ struct bpfcap_fpga_dev {
 
 static struct of_device_id bpfcap_fpga_dt_ids[] = {
     {
-        .compatible = "dev, bpfcap_fpga"
+        .compatible = "dev,bpfcap_fpga"
     },
     { /* end of table */ }
 };
@@ -63,16 +63,17 @@ static int bpfcap_fpga_probe(struct platform_device *pdev)
     struct bpfcap_fpga_dev *dev;
     struct resource *r = NULL;
 
-    //pr_info("bpfcap_fpga_probe enter \n");
+    pr_info("bpfcap_fpga_probe enter \n");
 
     r = platform_get_resource(pdev, IORESOURCE_MEM, 0);
     if (r == NULL) {
-        //pr_err("IORESOURCE_MEM (register space) does not exist!\n");
+        pr_err("IORESOURCE_MEM (register space) does not exist!\n");
         goto bad_exit_return;
     }
 
     dev = devm_kzalloc(&pdev->dev, sizeof(struct bpfcap_fpga_dev), GFP_KERNEL);
 
+    dev->regs = devm_ioremap_resource(&pdev->dev, r);
     if (IS_ERR(dev->regs))
         goto bad_ioremap;
 
@@ -87,31 +88,31 @@ static int bpfcap_fpga_probe(struct platform_device *pdev)
 
     ret_val = misc_register(&dev->miscdev);
     if (ret_val != 0) {
-        //pr_info("Could not register misc device\n");
+        pr_info("Could not register misc device\n");
         goto bad_exit_return;
     }
 
     platform_set_drvdata(pdev, (void*)dev);
 
-    //pr_info("bpfcap_fpga_probe exit\n");
+    pr_info("bpfcap_fpga_probe exit\n");
 
     return 0;
 bad_ioremap:
     ret_val = PTR_ERR(dev->regs);
 bad_exit_return:
-    //pr_info("bpfcap_fpga_probe bad exit\n");
+    pr_info("bpfcap_fpga_probe bad exit\n");
     return ret_val;
 }
 
 static int bpfcap_fpga_remove(struct platform_device *pdev)
 {
     struct bpfcap_fpga_dev *dev = (struct bpfcap_fpga_dev*)platform_get_drvdata(pdev);
-    //pr_info("bpfcap_fpga_remove enter\n");
+    pr_info("bpfcap_fpga_remove enter\n");
 
     // write stop to the capture program? TODO:
 
     misc_deregister(&dev->miscdev);
-    //pr_info("bpfcap_fpga_remove exit\n");
+    pr_info("bpfcap_fpga_remove exit\n");
     return 0;
 }
 
@@ -120,11 +121,11 @@ static ssize_t bpfcap_fpga_write(struct file *file, const char *buffer, size_t l
     int success = 0;
     struct bpfcap_fpga_dev *dev = container_of(file->private_data, struct bpfcap_fpga_dev, miscdev);
 
-    //pr_info("Writing to the device\n");
+    pr_info("Writing to the device\n");
 
     success = copy_from_user(&dev->write_value, buffer, sizeof(dev->write_value));
     if (success != 0) {
-        //pr_info("Failed to copy the register data\n");
+        pr_info("Failed to copy the register data\n");
     }
 
     return len;
@@ -154,25 +155,25 @@ static int bpfcap_fpga_mmap(struct file *file, struct vm_area_struct *vma)
 static int bpfcap_fpga_init(void)
 {
     int ret_val = 0;
-    //pr_info("Initializing the FPGA accelerated tcpdump driver module.\n");
+    pr_info("Initializing the FPGA accelerated tcpdump driver module.\n");
 
     ret_val = platform_driver_register(&bpfcap_fpga_platform);
     if (ret_val != 0) {
-        //pr_err("platform_driver_register returned %d\n", ret_val);
+        pr_err("platform_driver_register returned %d\n", ret_val);
         return ret_val;
     }
 
-    //pr_info("FPGA accelerated tcpdump driver module properly initialized!\n");
+    pr_info("FPGA accelerated tcpdump driver module properly initialized!\n");
     return 0;
 }
 
 static void bpfcap_fpga_exit(void)
 {
-    //pr_info("FPGA accelerated tcpdump driver module exit.\n");
+    pr_info("FPGA accelerated tcpdump driver module exit.\n");
 
     platform_driver_unregister(&bpfcap_fpga_platform);
 
-    //pr_info("FPGA accelerated tcpdump driver module successfully unregistered.\n");
+    pr_info("FPGA accelerated tcpdump driver module successfully unregistered.\n");
 }
 
 module_init(bpfcap_fpga_init);
